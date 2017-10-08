9231d7ae0ce8b40043e9e154453a9f9149668f84
signalflowsean <signalflowsean@gmail.com>
2017-10-08 12:26:06 -0400

    Finding maybe enclosed

--- Barricade/Assets/Scripts/Select.cs
+++ Barricade/Assets/Scripts/Select.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Select : MonoBehaviour {
     public FindNeighbors myNeighbors;
     public WallDetecter myWallDetect; 
-    public GameObject tower; 
+    public GameObject tower;
+    public List<Vector3> isClickedPositions; 
     //public Wall
 	// Use this for initialization
 	void Start () {
@@ -26,11 +27,13 @@ public class Select : MonoBehaviour {
                 //print(hit.transform.position); 
                 Instantiate(tower, new Vector3((float)hit.transform.position.x,(float) hit.transform.position.y,(float) hit.transform.position.z), new Quaternion(0, 0, 0, 0)); 
                 currSelected.GetComponent<Renderer>().material.color = Color.blue;
+
+                isClickedPositions.Add(new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z)); 
                 myNeighbors.calculateNeighbors(hit.transform.name);
                 //print (hit.transform.position.x + " a" +  hit.transform.position.y);
                 //print(currSelected.transform.position + "yo"); 
                 myWallDetect.FindSelected(currSelected.transform.localPosition.x, currSelected.transform.localPosition.y); 
-                
+               
 
             }
         }
